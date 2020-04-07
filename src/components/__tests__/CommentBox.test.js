4c454dc2ba689f92c96443c0e1e793bc65a41301
Frank Mendez <frankmendezwebdev@gmail.com>
2020-04-07 20:16:53 +0800

    AfterEach Statements

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -8,6 +8,12 @@ beforeEach(() => {
   wrapped = mount(<CommentBox />);
 });
 
+//common clean up code after every single test is executed component cleanup
+// unmount from fake DOM
+afterEach(() => {
+  wrapped.unmount();
+});
+
 it('has a textarea and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
