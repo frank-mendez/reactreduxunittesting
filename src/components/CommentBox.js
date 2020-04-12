77d178477c0cdd05cdd24a175ecbbb35923fd4cd
Frank Mendez <frankmendezwebdev@gmail.com>
2020-04-12 23:07:00 +0800

    Adding a Root Component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -11,6 +11,7 @@ class CommentBox extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
+    this.props.saveComment(this.state.comment);
     this.setState({ comment: '' });
   };
 
