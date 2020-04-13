cf3eb990457e6ad46537dd208a3578b413689777
Frank Mendez <frankmendezwebdev@gmail.com>
2020-04-14 00:32:33 +0800

    Cheerio Queries

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -18,4 +18,12 @@ beforeEach(() => {
   );
 });
 
-it('creates one LI per comment', () => {});
+it('creates one LI per comment', () => {
+  expect(wrapped.find('li').length).toEqual(2);
+});
+
+it('shows the text for each comment', () => {
+  //console.log(wrapped.render().text());
+  expect(wrapped.render().text()).toContain('Comment 1');
+  expect(wrapped.render().text()).toContain('Comment 2');
+});
