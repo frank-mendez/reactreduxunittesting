368e8f59c7f61ef49d85a7a1d6e8ae0c86860233
Frank Mendez <frankmendezwebdev@gmail.com>
2020-05-09 00:33:35 +0800

    Observing the Middleware

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -4,7 +4,8 @@ export default ({ dispatch }) => (next) => (action) => {
   //If it does, then wait for it to resolve
   //If it doesn't then send the action on to the
   //next middleware
-  if (!action.payload || action.payload.then) {
+
+  if (!action.payload || !action.payload.then) {
     return next(action);
   }
 
